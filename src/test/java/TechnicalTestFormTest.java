4bfdeb9edbd1feaa903f2d98d4061eec88543921
EthGoh <goh_chong_sheng@hotmail.com>
2024-08-04 13:18:31 +0800

    .

--- src/test/java/TechnicalTestFormTest.java
+++ src/test/java/TechnicalTestFormTest.java
@@ -1,4 +1,4 @@
-package govtech01;
+package test.java;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -8,6 +8,8 @@ import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import govtech01.TechnicalTestForm;
 
 public class TechnicalTestFormTest {
 
